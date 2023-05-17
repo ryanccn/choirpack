36c42ca04d7984428c491e584b2721267e563e7e
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-05-17 13:28:45 +0800

    chore: allow custom install directory in install script [skip ci]

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@ set -eo pipefail
 os="$(uname -s | tr '[:upper:]' '[:lower:]')"
 arch="$(uname -m)"
 
+install_dir="${CHOIRPACK_INSTALL_DIR:-"$HOME/.local/bin"}"
 binary_name="choirpack-$os-$arch"
 
 download_url="https://nightly.link/ryanccn/choirpack/workflows/build/main/choirpack-$os-$arch.zip"
 download_path="$(mktemp -d)"
 
 curl --location "$download_url" -o "$download_path/choirpack.zip"
-unzip "$download_path/choirpack.zip" -d "$HOME/.local/bin"
+unzip "$download_path/choirpack.zip" -d "$install_dir" &> /dev/null
 
-chmod +x "$HOME/.local/bin/$binary_name"
-mv "$HOME/.local/bin/$binary_name" "$HOME/.local/bin/choirpack"
+chmod +x "$install_dir/$binary_name"
+mv "$install_dir/$binary_name" "$install_dir/choirpack"
 rm -rf "$download_path"
 
-echo "Installed Choirpack to ~/.local/bin!"
+echo "Installed Choirpack to $install_dir!"
